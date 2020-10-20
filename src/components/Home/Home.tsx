15c18307454cd1413edaeabef8f99dcd25135c6b
MikeLD <mikeu76@gmail.com>
2020-10-20 20:17:22 +0200

    add router

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import logo from './../../logo.svg';
+
+export const Home = (): JSX.Element => (
+  <div>
+    <header>
+      <h1>Github Users App</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+    </header>
+  </div>
+)
---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import logo from './../../logo.svg';
+
+export const Users = (): JSX.Element => (
+  <div>
+    <h2>Users list</h2>
+    <img src={logo} className="App-logo" alt="logo" />
+  </div>
+)
